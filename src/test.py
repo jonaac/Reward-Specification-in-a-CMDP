3657876e0a8337fce279e1af2485e8ebf6639b58
Jonathan Azpur <jazpurc@gmail.com>
2021-09-18 19:56:05 -0400

    test file for google cola

--- src/test.py
+++ src/test.py
@@ -8,9 +8,4 @@ import matplotlib.pyplot as plt
 
 from rl.cont.sddpg import SDDPG
 
-observation_space = 10
-action_space = 4
-sddpg = SDDPG(observation_space, action_space, 1)
-
-s = np.random.rand(observation_space,)
-sddpg.act(s)
+print('Hello Google Drive')
