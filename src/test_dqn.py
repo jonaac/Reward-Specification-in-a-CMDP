f4c8103fb6ae93e82ebcd1a61d14d734a56790bb
Jonathan Azpur <jazpurc@gmail.com>
2021-09-19 22:56:34 -0400

    update test script

--- src/test_dqn.py
+++ src/test_dqn.py
@@ -1,65 +1,68 @@
 import time
 import sys
-import psutil
+from argparse import ArgumentParser
 
-import envs
 import gym
-import rl
-
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
 from rl.disc.dqn import DQL
 from rl.dqn_parameters import (
-	CHECKPOINTS_PATH, TOTAL_EPISODES,
+	CHECKPOINTS_PATH, TOTAL_STEPS,
 	TF_LOG_DIR, UNBALANCE_P)
 
-gym.logger.set_level(40)
-SAVE_WEIGHTS = True
 
-env = gym.make('Safe-Water-World-NoMachine-v0')
-observation_space = env.observation_space.shape[0]
-action_space = env.action_space.n
+def main():
 
-dql = DQL(observation_space, action_space)
+	parser = ArgumentParser(
+		description="Deep Q-Learning with Safety Machines"
+	)
+	parser.add_argument('--env', type=str, nargs='?',
+						default="Safe-Water-World-NoMachine-v0",
+						help='The OpenAI Gym environment to train on, '
+							 'e.g. Safe-Water-World-NoMachine-v0, Safe-Water-World-RewardMachine-v0'
+							 ' Safe-Water-World-SafetyMachine-v0')
+	parser.add_argument('--crm', type=bool, nargs='?', default=False,
+						help='Add reward machine/safety machine states to replay buffer')
+	parser.add_argument('--save_weights', type=bool, nargs='?', default=True,
+						help='Save the weight of the network in the defined checkpoint file '
+							 'directory.')
 
-s = env.reset()
-print(s.shape)
+	args = parser.parse_args()
+	RL_TASK = args.env
+	SAVE_WEIGHTS = args.save_weights
+	CRM = args.crm
 
-exit()
-# weighted sum of rewards for each epoch
-acc_reward = tf.keras.metrics.Sum('reward',dtype=tf.float32)
-acc_cost = tf.keras.metrics.Sum('cost',dtype=tf.float32)
+	env = gym.make(RL_TASK)
+	observation_space = env.observation_space.shape[0]
+	action_space = env.action_space.n
 
-# To store reward history of each episode
-ep_reward_list = []
-# To store average reward history of last few episodes
-avg_reward_list = []
+	dql = DQL(observation_space, action_space)
 
-
-EPOCHS = 2000
-T = 500
-
-crm = False
-
-print('Start Training')
-print('--------------')
-
-for epoch in range(EPOCHS):
 	s = env.reset()
-	acc_reward.reset_states()
-	acc_cost.reset_states()
-	dql.noise.reset()
-	print('Epoch #' + str(epoch))
-	time_load = []
-	time_train = []
 
-	for t in range(T):
+	# weighted sum of rewards for each epoch
+	acc_reward = tf.keras.metrics.Sum('reward', dtype=tf.float32)
+	acc_cost = tf.keras.metrics.Sum('cost', dtype=tf.float32)
+
+	# To store reward history of each episode
+	ep_reward_list = []
+	ep_cost_list = []
+	# To store average reward history of last few episodes
+	avg_reward_list = []
+	avg_cost_list = []
+
+	print('* ----- Start Training ----- ')
+	print('-----------------------------')
+
+	step_count, epoch_count = 0, 0
+	for t in TOTAL_STEPS:
+		step_count += 1
 		a, q_l = dql.act(s)
 		sn, r, d, done, info = env.step(a)
 
-		if crm:
+		if CRM:
 			experiences = info['crm-experience']
 			dql.remember(experiences)
 		else:
@@ -69,33 +72,37 @@ for epoch in range(EPOCHS):
 			entry = dql.buffer.get_batch(unbalance_p=UNBALANCE_P)
 			q = dql.learn(entry)
 
-		acc_reward(r)
-		acc_cost(d)
-		s = sn
-
 		if done:
-			print("Episode finished after {} timesteps".format(t+1))
-			break
+			print("Episode {} finished after {} time-steps".format(epoch_count, step_count))
+			print("Currently at {} total time-steps".format(epoch_count, step_count))
+			print('-----------------------------')
+			s = env.reset()
+			acc_reward.reset_states()
+			acc_cost.reset_states()
+			epoch_count += 1
+			step_count = 0
 
-	ep_reward_list.append(acc_reward.result().numpy())
-	# Mean of reward obtained in last 40 steps fo training
-	if epoch >= 40:
-		avg_reward = np.mean(ep_reward_list[-40:])
-		print('Average Reward of last 40 steps: ' + str(avg_reward))
-		# add to list to plot
-		avg_reward_list.append(avg_reward)
+		ep_reward_list.append(acc_reward.result().numpy())
+		# Mean of reward obtained in last 40 steps fo training
+		if epoch_count >= 40:
+			avg_reward = np.mean(ep_reward_list[-40:])
+			# add to list to plot
+			avg_reward_list.append(avg_reward)
 
-	# save weights
-	if (epoch % 5 == 0 and
-		SAVE_WEIGHTS and
-		epoch >= 5):
+		# save weights
+		if (epoch_count % 100 == 0 and
+			epoch_count >= 100 and
+			SAVE_WEIGHTS):
+			dql.save_weights(CHECKPOINTS_PATH)
 
-		dql.save_weights(CHECKPOINTS_PATH)
+	env.close()
+	dql.save_weights(CHECKPOINTS_PATH)
 
-env.close()
-dql.save_weights(CHECKPOINTS_PATH)
+	plt.plot(avg_reward_list)
+	plt.xlabel("Episode")
+	plt.ylabel("Avg. Epsiodic Reward")
+	plt.show()
 
-plt.plot(avg_reward_list)
-plt.xlabel("Episode")
-plt.ylabel("Avg. Epsiodic Reward")
-plt.show()
+
+if __name__ == '__main__':
+	main()
